af61eeb272568a0c04e33d872838919a01e12a7f
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-02-19 10:10:36 +0100

    genarate html throught pug

--- webpack.config.js
+++ webpack.config.js
@@ -29,12 +29,20 @@ module.exports = {
           filename: 'images/[name][ext]'
         },
         use: [
-          // {
-          //   loader: MiniCssExtractPlugin.loader,
-          // },
-          // {
-          //   loader: 'css-loader',
-          // }
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            }
+          }
         ],
       },
     ],
@@ -44,7 +52,12 @@ module.exports = {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
